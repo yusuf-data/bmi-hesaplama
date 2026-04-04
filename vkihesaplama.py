82c43e1a186e106fcf47838c9dec8e888e431eb3
Yusuf Özdemir <yusufzdemr2@gmail.com>
2026-04-04 23:21:56 +0300

    öneri sistemi eklendi

--- vkihesaplama.py
+++ vkihesaplama.py
@@ -2,9 +2,11 @@ kilo = input("Kilonuzu giriniz:")
 boy = input("Boyunuzu metre olarak giriniz:")
 bmi = float(kilo) / (float(boy) ** 2)
 if bmi < 18.5:
-    print("Zayıfsıınız, BMI:", bmi)
+    print("Zayıfsıınız, kilo almanız önerilir, BMI:")
 elif  bmi > 18.5 and bmi < 24.9:
-    print("Normal kilodasınız, BMI:", bmi)
+    print("Normal kilodasınız, BMI:")
 elif  bmi > 25 and bmi < 29.9:
-    print("Fazla kilolusunuz, BMI:", bmi)
-else:   print("Obezsiniz, BMI:", bmi)
+    print("Fazla kilolusunuz, sağlıklı beslenmeniz önerilir, BMI:")
+else:   print("Obezsiniz, diyetisyen ile görüşmeniz önerilir, BMI:")
+bmi = round(bmi, 2)
+print(bmi)
